c444192dee05073fb2f04f1c59c3857ab60e6375
Shobhit Mishra <46920235+shobhu29@users.noreply.github.com>
2019-02-26 16:17:14 +0530

    Add files via upload

---
+++ ScopeServletExample/src/com/capg/serv/MyServlet.java
@@ -0,0 +1,61 @@
+package com.capg.serv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("name");
+		HttpSession session = request.getSession();
+		ServletContext context = getServletContext();
+		
+		if(name!=null){
+			
+			request.setAttribute("request-name", name);
+			session.setAttribute("session-name", name);
+			context.setAttribute("context-name", name);
+		}
+		
+		out.println(request.getAttribute("request-name"));
+		out.println(session.getAttribute("session-name"));
+		out.println(context.getAttribute("context-name"));
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
